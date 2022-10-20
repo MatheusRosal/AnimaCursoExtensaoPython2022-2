06995c712cd61147c09abc162cac7dfe90595598
Matheus Rosal <68031034+MatheusRosal@users.noreply.github.com>
2022-10-20 01:00:31 +0000

    Como criar uma variável

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 '''
 Comentario em bloco
 '''
+
 # Meu primeiro projeto Python
 # Print('Hello World!') = comando de saida
 print('Hello World!')
 
+# Quando quiser guardar uma String! (frase)
+nome = 'Matheus Rosal'
+# Quando quiser guardar um número inteiro
+idade = 19
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
