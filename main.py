cd7cd4b6222ae2a014b09fcf432a012825710e13
Matheus Rosal <68031034+MatheusRosal@users.noreply.github.com>
2022-10-20 01:21:01 +0000

    Aprendemos a exibir variáveis de diferentes formas

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ nome = 'Matheus Rosal'
 idade = 19
 # Exibir o meu nome (que está dentro da variável nome)
 print(nome)
+# Quando quiser exibir uma frase completando com o conteúdo de alguma variável
+print(f'O meu nome é {nome}\n')
+print(f'Minha idade é {idade} anos')
+# Quando quiser exibir 'Meu nome é... e tenho... anos'
+print('Meu nome é {} e tenho {} anos'.format(nome, idade))
