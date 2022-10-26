373bb8b082560109c0269d5df442504ad1b1fe26
Matheus Rosal <68031034+MatheusRosal@users.noreply.github.com>
2022-10-26 23:15:24 +0000

    Ensinei a usar input e pedir algum dado ao usuário

---
+++ aula1-2022-10-29.py
@@ -0,0 +1,19 @@
+'''
+Comentario em bloco
+'''
+
+# Meu primeiro projeto Python
+# Print('Hello World!') = comando de saida
+print('Hello World!')
+
+# Quando quiser guardar uma String! (frase)
+nome = 'Matheus Rosal'
+# Quando quiser guardar um número inteiro
+idade = 19
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+# Quando quiser exibir uma frase completando com o conteúdo de alguma variável
+print(f'O meu nome é {nome}\n')
+print(f'Minha idade é {idade} anos')
+# Quando quiser exibir 'Meu nome é... e tenho... anos'
+print('Meu nome é {} e tenho {} anos'.format(nome, idade))
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-'''
-Comentario em bloco
-'''
-
-# Meu primeiro projeto Python
-# Print('Hello World!') = comando de saida
-print('Hello World!')
-
-# Quando quiser guardar uma String! (frase)
-nome = 'Matheus Rosal'
-# Quando quiser guardar um número inteiro
-idade = 19
-# Exibir o meu nome (que está dentro da variável nome)
-print(nome)
-# Quando quiser exibir uma frase completando com o conteúdo de alguma variável
-print(f'O meu nome é {nome}\n')
-print(f'Minha idade é {idade} anos')
-# Quando quiser exibir 'Meu nome é... e tenho... anos'
-print('Meu nome é {} e tenho {} anos'.format(nome, idade))
+# comando input(): permitir que o usuario digite algo.
+nome = input('Qual seu nome: ')
+idade = int(input('Digite sua idade: '))
+print(f'Bem-vindo {nome}.')
+print(f'Sua idade é {idade} anos')
+print(f'O dobro da sua idade é {2 * idade} anos')
