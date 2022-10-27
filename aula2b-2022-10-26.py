392202935838cfa0a25d27fe2b7a745cbc5dbd2d
Matheus Rosal <68031034+MatheusRosal@users.noreply.github.com>
2022-10-27 01:08:30 +0000

    Ensinei a usar o elif

---
+++ aula2b-2022-10-26.py
@@ -0,0 +1,8 @@
+nome = input('Qual seu nome: ')
+nota = float(input('Sua nota: '))
+if nota == 10:
+  print('Você é o BICHÃO mesmo!!!')
+elif nota >= 7:
+  print('Ok, está na média :)')
+else:
+  print('Nada bom :C')
