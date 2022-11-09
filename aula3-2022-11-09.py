29dfb9dff2615b505c0afdfb7d40f4b19925209d
Matheus Rosal <68031034+MatheusRosal@users.noreply.github.com>
2022-11-09 23:59:19 +0000

    Como fazer laços (while, for)

---
+++ aula3-2022-11-09.py
@@ -0,0 +1,17 @@
+n = 0
+while n <= 10:
+  print(n)
+  n += 1
+
+frutas = ['Morango', 'Laranja', 'Pêra']
+print(frutas[2])
+frutas.append('Manga')
+i = 0
+print('Exemnplo com while')
+while i < len(frutas):
+  print(frutas[i])
+  i += 1
+print('Exemplo com for')
+for c in frutas:
+  print(c)
+  
